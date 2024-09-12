df189fbe114ef5bbc941e8a62a5ec07e6fe051f2
viniciusyag06 <159543201+viniciusyag06@users.noreply.github.com>
2024-09-12 12:25:27 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá Mundo";
+     const texto2 = "Olá Mundo";
+     const senha = "senhaSuperSegura456!";
+     const stringDeNumeros = "34567";
+
+     const citacao = 'Meu nome é ';
+     const meuNome = 'Vinícius';
+     
+     console.log(citacao + meuNome)
+
+
+     // concatenacao - juntar duas ou mais variaveis (textos, operadores, variaveis)
+     // template string ou temple literal
